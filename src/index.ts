d97943538d2f97d67e801e6cdd31e4a4af7ffb25
Stefan Stoichev <stefan.stoichev@gmail.com>
2022-09-09 04:42:30 +0100

    docs; top level export object change

--- src/index.ts
+++ src/index.ts
@@ -13,200 +13,200 @@ import * as schema from "./schema/root.json";
 
 import draft from "ajv/dist/refs/json-schema-draft-06.json";
 
-export namespace TestOMatiq {
-  export class client {
-    specs: Root;
-    emitter: EventsBus;
-    testResults: IGroupResult[];
-    qlikApp: IAppMixin;
-    testGroups: string[];
+// export namespace TestOMatiq {
+export class TestOMatiq {
+  specs: Root;
+  emitter: EventsBus;
+  testResults: IGroupResult[];
+  qlikApp: IAppMixin;
+  testGroups: string[];
 
-    constructor(specs: Root, qlikApp: IAppMixin) {
-      this.specs = specs;
-      this.qlikApp = qlikApp;
-      this.emitter = new EventsBus();
-      this.testResults = [];
+  constructor(specs: Root, qlikApp: IAppMixin) {
+    this.specs = specs;
+    this.qlikApp = qlikApp;
+    this.emitter = new EventsBus();
+    this.testResults = [];
 
-      this.testGroups = Object.entries(specs.spec).map((value) =>
-        value[0].toString()
-      );
+    this.testGroups = Object.entries(specs.spec).map((value) =>
+      value[0].toString()
+    );
 
-      const ajv = new Ajv({
-        strict: true,
-        allowUnionTypes: true,
-        allErrors: true,
-      });
-      ajv.addMetaSchema(draft);
+    const ajv = new Ajv({
+      strict: true,
+      allowUnionTypes: true,
+      allErrors: true,
+    });
+    ajv.addMetaSchema(draft);
 
-      const isValidSpec = ajv.validate(schema, specs);
+    const isValidSpec = ajv.validate(schema, specs);
 
-      if (isValidSpec == false)
-        throw {
-          message: "Error(s) while validating the input",
-          errors: ajv.errors,
-        };
-    }
-
-    async run(): Promise<IGroupResult[]> {
-      if (this.specs.selections) {
-        this.emitter.emit("all", {
-          message: `Applying selections in ${this.specs.selections.length} field(s)`,
-        });
-
-        const selection = new Selection(this.specs.selections, this.qlikApp);
-        const makeSelections = await selection.makeSelections();
-
-        this.emitter.emit("all", {
-          message: `Selections applied`,
-        });
-      }
-
-      if (this.specs.spec.Meta) {
-        this.emitter.emit("group", {
-          group: "Meta",
-          message: `Starting Meta tests ...`,
-          isFinished: false,
-          status: true,
-          elapsedTime: -1,
-          totalTests: -1,
-          failedTests: -1,
-        });
-
-        const meta = new Meta(this.specs.spec.Meta || undefined, this.qlikApp);
-        const metaResult = await meta.run();
-
-        this.emitter.emit("group", {
-          group: "Meta",
-          message: `Meta tests finished`,
-          isFinished: true,
-          status: metaResult.status,
-          elapsedTime: metaResult.elapsedTime,
-          totalTests: metaResult.totalTests,
-          failedTests: metaResult.failedTests,
-        });
-        this.emitter.emit("group:result", metaResult);
-
-        this.testResults.push(metaResult);
-      }
-
-      if (this.specs.spec.Scalar) {
-        this.emitter.emit("group", {
-          group: "Scalar",
-          message: `Starting Scalar tests ...`,
-          isFinished: false,
-          status: true,
-          elapsedTime: -1,
-          totalTests: -1,
-          failedTests: -1,
-        });
-
-        const scalar = new Scalar(this.specs.spec.Scalar, this.qlikApp);
-        const scalarResult = await scalar.run();
-
-        this.emitter.emit("group", {
-          group: "Scalar",
-          message: `Scalar tests finished`,
-          isFinished: true,
-          status: scalarResult.status,
-          elapsedTime: scalarResult.elapsedTime,
-          totalTests: scalarResult.totalTests,
-          failedTests: scalarResult.failedTests,
-        });
-        this.emitter.emit("group:result", scalarResult);
-
-        this.testResults.push(scalarResult);
-      }
-
-      if (this.specs.spec.List) {
-        this.emitter.emit("group", {
-          group: "List",
-          message: `Starting Objects tests ...`,
-          isFinished: false,
-          status: true,
-          elapsedTime: -1,
-          totalTests: -1,
-          failedTests: -1,
-        });
-
-        const list = new List(this.specs.spec.List || undefined, this.qlikApp);
-        const listResult = await list.run();
-
-        this.emitter.emit("group", {
-          group: "List",
-          message: `Objects tests finished`,
-          isFinished: true,
-          status: listResult.status,
-          elapsedTime: listResult.elapsedTime,
-          totalTests: listResult.totalTests,
-          failedTests: listResult.failedTests,
-        });
-        this.emitter.emit("group:result", listResult);
-
-        this.testResults.push(listResult);
-      }
-
-      if (this.specs.spec.Object) {
-        this.emitter.emit("group", {
-          group: "Objects",
-          message: `Starting Objects tests ...`,
-          isFinished: false,
-          status: true,
-          elapsedTime: -1,
-          totalTests: -1,
-          failedTests: -1,
-        });
-
-        const qObject = new QObject(this.specs.spec.Object, this.qlikApp);
-        const objectExistsResult = await qObject.run();
-
-        this.emitter.emit("group", {
-          group: "Objects",
-          message: `Objects tests finished`,
-          isFinished: true,
-          status: objectExistsResult.status,
-          elapsedTime: objectExistsResult.elapsedTime,
-          totalTests: objectExistsResult.totalTests,
-          failedTests: objectExistsResult.failedTests,
-        });
-        this.emitter.emit("group:result", objectExistsResult);
-
-        this.testResults.push(objectExistsResult);
-      }
-
-      if (this.specs.spec.Table) {
-        this.emitter.emit("group", {
-          group: "Table",
-          message: `Starting Table tests ...`,
-          isFinished: false,
-          status: true,
-          elapsedTime: -1,
-          totalTests: -1,
-          failedTests: -1,
-        });
-
-        const table = new Table(this.specs.spec.Table, this.qlikApp);
-        const tableResult = await table.run();
-
-        this.emitter.emit("group", {
-          group: "Table",
-          message: `Table tests finished`,
-          isFinished: true,
-          status: tableResult.status,
-          elapsedTime: tableResult.elapsedTime,
-          totalTests: tableResult.totalTests,
-          failedTests: tableResult.failedTests,
-        });
-        this.emitter.emit("group:result", tableResult);
-
-        this.testResults.push(tableResult);
-      }
-
-      return this.testResults.flat();
-    }
-
-    // private getTimeStamp() {
-    //   const d = new Date();
-    //   return d.toLocaleString();
-    // }
+    if (isValidSpec == false)
+      throw {
+        message: "Error(s) while validating the input",
+        errors: ajv.errors,
+      };
   }
+
+  async run(): Promise<IGroupResult[]> {
+    if (this.specs.selections) {
+      this.emitter.emit("all", {
+        message: `Applying selections in ${this.specs.selections.length} field(s)`,
+      });
+
+      const selection = new Selection(this.specs.selections, this.qlikApp);
+      const makeSelections = await selection.makeSelections();
+
+      this.emitter.emit("all", {
+        message: `Selections applied`,
+      });
+    }
+
+    if (this.specs.spec.Meta) {
+      this.emitter.emit("group", {
+        group: "Meta",
+        message: `Starting Meta tests ...`,
+        isFinished: false,
+        status: true,
+        elapsedTime: -1,
+        totalTests: -1,
+        failedTests: -1,
+      });
+
+      const meta = new Meta(this.specs.spec.Meta || undefined, this.qlikApp);
+      const metaResult = await meta.run();
+
+      this.emitter.emit("group", {
+        group: "Meta",
+        message: `Meta tests finished`,
+        isFinished: true,
+        status: metaResult.status,
+        elapsedTime: metaResult.elapsedTime,
+        totalTests: metaResult.totalTests,
+        failedTests: metaResult.failedTests,
+      });
+      this.emitter.emit("group:result", metaResult);
+
+      this.testResults.push(metaResult);
+    }
+
+    if (this.specs.spec.Scalar) {
+      this.emitter.emit("group", {
+        group: "Scalar",
+        message: `Starting Scalar tests ...`,
+        isFinished: false,
+        status: true,
+        elapsedTime: -1,
+        totalTests: -1,
+        failedTests: -1,
+      });
+
+      const scalar = new Scalar(this.specs.spec.Scalar, this.qlikApp);
+      const scalarResult = await scalar.run();
+
+      this.emitter.emit("group", {
+        group: "Scalar",
+        message: `Scalar tests finished`,
+        isFinished: true,
+        status: scalarResult.status,
+        elapsedTime: scalarResult.elapsedTime,
+        totalTests: scalarResult.totalTests,
+        failedTests: scalarResult.failedTests,
+      });
+      this.emitter.emit("group:result", scalarResult);
+
+      this.testResults.push(scalarResult);
+    }
+
+    if (this.specs.spec.List) {
+      this.emitter.emit("group", {
+        group: "List",
+        message: `Starting Objects tests ...`,
+        isFinished: false,
+        status: true,
+        elapsedTime: -1,
+        totalTests: -1,
+        failedTests: -1,
+      });
+
+      const list = new List(this.specs.spec.List || undefined, this.qlikApp);
+      const listResult = await list.run();
+
+      this.emitter.emit("group", {
+        group: "List",
+        message: `Objects tests finished`,
+        isFinished: true,
+        status: listResult.status,
+        elapsedTime: listResult.elapsedTime,
+        totalTests: listResult.totalTests,
+        failedTests: listResult.failedTests,
+      });
+      this.emitter.emit("group:result", listResult);
+
+      this.testResults.push(listResult);
+    }
+
+    if (this.specs.spec.Object) {
+      this.emitter.emit("group", {
+        group: "Objects",
+        message: `Starting Objects tests ...`,
+        isFinished: false,
+        status: true,
+        elapsedTime: -1,
+        totalTests: -1,
+        failedTests: -1,
+      });
+
+      const qObject = new QObject(this.specs.spec.Object, this.qlikApp);
+      const objectExistsResult = await qObject.run();
+
+      this.emitter.emit("group", {
+        group: "Objects",
+        message: `Objects tests finished`,
+        isFinished: true,
+        status: objectExistsResult.status,
+        elapsedTime: objectExistsResult.elapsedTime,
+        totalTests: objectExistsResult.totalTests,
+        failedTests: objectExistsResult.failedTests,
+      });
+      this.emitter.emit("group:result", objectExistsResult);
+
+      this.testResults.push(objectExistsResult);
+    }
+
+    if (this.specs.spec.Table) {
+      this.emitter.emit("group", {
+        group: "Table",
+        message: `Starting Table tests ...`,
+        isFinished: false,
+        status: true,
+        elapsedTime: -1,
+        totalTests: -1,
+        failedTests: -1,
+      });
+
+      const table = new Table(this.specs.spec.Table, this.qlikApp);
+      const tableResult = await table.run();
+
+      this.emitter.emit("group", {
+        group: "Table",
+        message: `Table tests finished`,
+        isFinished: true,
+        status: tableResult.status,
+        elapsedTime: tableResult.elapsedTime,
+        totalTests: tableResult.totalTests,
+        failedTests: tableResult.failedTests,
+      });
+      this.emitter.emit("group:result", tableResult);
+
+      this.testResults.push(tableResult);
+    }
+
+    return this.testResults.flat();
+  }
+
+  // private getTimeStamp() {
+  //   const d = new Date();
+  //   return d.toLocaleString();
+  // }
 }
+// }
--- src/util/EventBus.ts
+++ src/util/EventBus.ts
@@ -3,7 +3,6 @@ import {
   IEventError,
   IEventGroupStartEnd,
   IGroupResult,
-  ITestResponse,
 } from "../interface/Specs";
 import TypedEmitter from "typed-emitter";
 
@@ -11,7 +10,6 @@ type MessageEvents = {
   testError: (error: IEventError) => void;
   all: (body: { message: string }) => void;
   group: (body: IEventGroupStartEnd) => void;
-  // "group:end": (body: IEventGroupStartEnd) => void;
   "group:result": (body: IGroupResult) => void;
 };
 
@@ -25,29 +23,3 @@ export class EventsBus extends (EventEmitter as new () => TypedEmitter<MessageEv
     EventsBus.instance = this;
   }
 }
-
-// export declare interface EventsBus {
-//   on(event: "selections", listener: (name: string) => void): this;
-//   on(event: "all", listener: (event: ITestResponse) => void): this;
-//   on(event: "task:result", listener: (name: string) => void): this;
-//   on(event: "group:start", listener: (name: string) => void): this;
-//   on(event: "group:end", listener: (name: string) => void): this;
-//   on(event: "error", listener: (name: string) => void): this;
-//   on(event: "debug", listener: (name: string) => void): this;
-//   emit(event: string | symbol, ...args: any[]): boolean;
-// }
-
-// export class EventsBus extends EventEmitter {
-//   private static instance: EventsBus;
-//   constructor() {
-//     super();
-//     if (EventsBus.instance) {
-//       return EventsBus.instance;
-//     }
-//     EventsBus.instance = this;
-//   }
-
-//   send(channels: string[], data: ITestResponse[]) {
-//     channels.map((channel) => this.emit(channel, data));
-//   }
-// }
